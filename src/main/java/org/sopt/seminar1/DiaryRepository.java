c3ddf6f5b167b110e894916e8858f468e3f000f0
ivoryeee <psa109@sookmyung.ac.kr>
2024-10-14 16:36:43 +0900

    [Feat] 일기 수정 횟수 제한 기능 구현

--- src/main/java/org/sopt/seminar1/DiaryRepository.java
+++ src/main/java/org/sopt/seminar1/DiaryRepository.java
@@ -3,6 +3,7 @@ package org.sopt.seminar1;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.time.LocalDate;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -10,6 +11,9 @@ public class DiaryRepository {
     private final Map<Long, String> storage = new ConcurrentHashMap<>();
     private final Map<Long, String> deletedStorage = new ConcurrentHashMap<>();
     private final AtomicLong numbering = new AtomicLong();
+    private final AtomicLong patchCount = new AtomicLong();
+    private final LocalDate initialDate = LocalDate.now();
+    private LocalDate firstPatchDate = initialDate;
 
     void save(final Diary diary){
         final long id = numbering.addAndGet(1);
@@ -35,10 +39,24 @@ public class DiaryRepository {
         storage.remove(id);
     }
 
-    void patch(final Long id,final String body){
-        final List<Diary> diaryList = new ArrayList<>();
-        storage.put(id, body);
-    }
+
+  void patch(final Long id,final String body){
+      final LocalDate currentPatchDate =  LocalDate.now();
+      if (!firstPatchDate.isEqual(currentPatchDate)) {
+          patchCount.set(0);
+          firstPatchDate = LocalDate.now() ;
+      }
+          if (patchCount.get() < 2){
+            if (patchCount.get() == 0){
+                  firstPatchDate=LocalDate.now();
+              }
+              storage.put(id, body);
+              patchCount.addAndGet(1);
+              System.out.println("일일 일기 수정 잔여 횟수는 "+ (2-patchCount.get())+"회입니다!");
+          } else{
+              System.out.println("일기 수정은 하루에 2번까지 가능합니다");
+          }
+      }
 
     void restore(final Long id){
         final String body = deletedStorage.get(id);
